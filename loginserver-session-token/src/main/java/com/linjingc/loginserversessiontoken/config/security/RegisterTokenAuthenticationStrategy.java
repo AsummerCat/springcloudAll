f4867690a1f0ce6dfb4b4cdfbb3419bb6ced4dc6
chenxiaocehng <583188551@qq.com>
2019-07-01 14:38:16 +0800

    登录服务 整合session和token一起使用验证

--- loginserver-session-token/src/main/java/com/linjingc/loginserversessiontoken/config/security/RegisterTokenAuthenticationStrategy.java
+++ loginserver-session-token/src/main/java/com/linjingc/loginserversessiontoken/config/security/RegisterTokenAuthenticationStrategy.java
@@ -1,7 +1,9 @@
 package com.linjingc.loginserversessiontoken.config.security;
 
 import com.linjingc.loginserversessiontoken.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,7 @@ import java.util.UUID;
  * @author cxc
  * @date 2019年7月1日14:26:51
  */
+@Slf4j
 @Component
 public class RegisterTokenAuthenticationStrategy implements SessionAuthenticationStrategy {
     private JwtUtils jwtUtils;
@@ -26,8 +29,9 @@ public class RegisterTokenAuthenticationStrategy implements SessionAuthenticatio
 
     @Override
     public void onAuthentication(Authentication authentication, HttpServletRequest request, HttpServletResponse response) throws SessionAuthenticationException {
-        System.out.println("heihei");
-        String token = jwtUtils.createJWT(UUID.randomUUID().toString(), "heihei", "heiehi");
+        log.info("--------------------------SecurityConfig自定义token会话认证加载----------------------------");
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String token = jwtUtils.createJWT(UUID.randomUUID().toString(), principal.getUsername() + "登录成功", principal.getUsername());
         // 但是这里创建的token只是单纯的token
         // 按照jwt的规定，最后请求的格式应该是 `Bearer token`
         response.setHeader(jwtUtils.TOKEN_HEADER, jwtUtils.TOKEN_PREFIX + token);
