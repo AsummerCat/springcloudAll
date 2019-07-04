d65ef73ab35552695b00c6cf43d47bbc87c21a7b
chenxiaocehng <583188551@qq.com>
2019-07-04 17:52:46 +0800

    oauth2实现单点登录

--- loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/LoginzuuloauthApplication.java
+++ loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/LoginzuuloauthApplication.java
@@ -2,12 +2,29 @@ package com.linjingc.loginzuuloauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
+@EnableZuulProxy
 public class LoginzuuloauthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginzuuloauthApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 }
---
+++ loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,50 @@
+package com.linjingc.loginzuuloauth.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * 授权服务中心
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        super.configure(clients);
+    }
+
+
+
+
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints
+                .tokenStore(new RedisTokenStore(redisConnectionFactory))
+                .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+        //允许表单认证
+        security.allowFormAuthenticationForClients();
+    }
+
+}
