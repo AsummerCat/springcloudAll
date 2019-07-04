38f0f83e6d121ea969311712b3a74745f739294f
chenxiaocehng <583188551@qq.com>
2019-07-04 16:56:59 +0800

    oauth2实现单点登录

---
+++ loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/LoginzuuloauthApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.loginzuuloauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginzuuloauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginzuuloauthApplication.class, args);
+    }
+
+}
