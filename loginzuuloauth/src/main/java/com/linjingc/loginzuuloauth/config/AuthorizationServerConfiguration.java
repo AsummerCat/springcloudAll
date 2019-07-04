a21cb723fea8e8fd58cb917cc0d6e82888be4eb2
chenxiaocehng <583188551@qq.com>
2019-07-04 18:06:28 +0800

    oauth2实现单点登录

--- loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/config/AuthorizationServerConfiguration.java
+++ loginzuuloauth/src/main/java/com/linjingc/loginzuuloauth/config/AuthorizationServerConfiguration.java
@@ -2,7 +2,9 @@ package com.linjingc.loginzuuloauth.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
